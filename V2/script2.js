47e95769767e975c1812614321e23dabed00e59e
Smulsaus <Yourimulders2006@hotmail.com>
2024-05-29 13:58:23 +0200

    update

--- V2/script2.js
+++ V2/script2.js
@@ -1,4 +1,5 @@
 // Selecting elements
+const gameModeSelect = document.querySelector("#gameMode");
 const computerOutput = document.querySelector("#computer");
 const humanOutput = document.querySelector("#human");
 const resultOutput = document.querySelector("#result");
@@ -16,6 +17,7 @@ let streak = 0;
 let round = 1;
 let playerScore = 0;
 let computerScore = 0;
+let gameMode = "bestOf3"; // Default game mode
 
 // Function to get computer choice
 function getComputerChoice() {
@@ -30,11 +32,11 @@ function determineWinner(humanChoice, computerChoice) {
     }
     switch (humanChoice) {
         case "Rock":
-            return computerChoice === "Scissors" ? "You win!" : "You lose!";
+            return computerChoice === "Scissors" ? "You won!" : "You lose!";
         case "Paper":
-            return computerChoice === "Rock" ? "You win!" : "You lose!";
+            return computerChoice === "Rock" ? "You won!" : "You lose!";
         case "Scissors":
-            return computerChoice === "Paper" ? "You win!" : "You lose!";
+            return computerChoice === "Paper" ? "You won!" : "You lose!";
     }
 }
 
@@ -47,7 +49,7 @@ function playGame(humanChoice) {
     resultOutput.innerHTML = result;
 
     // Update scores
-    if (result === "You win!") {
+    if (result === "You won!") {
         streak++;
         highscore = Math.max(highscore, streak);
         playerScore++;
@@ -55,18 +57,51 @@ function playGame(humanChoice) {
         highscoreOutput.innerHTML = highscore;
         streakOutput.innerHTML = streak;
         winSound.play();
+
+        // Check if player wins 3 times in best of 3 mode
+        if (gameMode === "bestOf3" && playerScore === 3) {
+            // Show confetti
+            // You can add confetti animation here or trigger it from a library
+            alert("Congratulations! You won the best of 3 round!");
+            resetGame();
+        }
     } else if (result === "You lose!") {
         streak = 0;
         computerScore++;
         computerScoreOutput.innerHTML = computerScore;
         streakOutput.innerHTML = streak;
         loseSound.play();
+
+        // Check if computer wins 3 times in best of 3 mode
+        if (gameMode === "bestOf3" && computerScore === 3) {
+            alert("You lost the best of 3 round!");
+            resetGame();
+        }
     }
 
     // Update round
     round++;
     roundOutput.innerHTML = round;
 }
+
+// Function to reset the game
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    round = 1;
+    playerScoreOutput.innerHTML = playerScore;
+    computerScoreOutput.innerHTML = computerScore;
+    roundOutput.innerHTML = round;
+}
+
+// Add event listener to game mode select
+gameModeSelect.addEventListener("change", function () {
+    gameMode = gameModeSelect.value;
+    // Reset game variables if needed based on game mode
+    if (gameMode === "bestOf3") {
+        resetGame();
+    }
+});
 
 // Add event listeners to buttons
 document.querySelectorAll("button").forEach(button => {
